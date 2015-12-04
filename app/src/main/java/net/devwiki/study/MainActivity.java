e7c15850649c429957d8adfdb2d32929a3cec7f2
asia <bozhou2010@139.com>
2015-12-05 01:24:51 +0800

    添加录音界面

--- app/src/main/java/net/devwiki/study/MainActivity.java
+++ app/src/main/java/net/devwiki/study/MainActivity.java
@@ -4,30 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-import net.devwiki.study.log.DevLog;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        button = (Button) findViewById(R.id.btn_context_info);
-        button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        Intent intent;
         switch (v.getId()){
             case R.id.btn_context_info:
-                Intent intent = new Intent(MainActivity.this, ContextActivity.class);
+                intent = new Intent(MainActivity.this, ContextActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.btn_record:
+                intent = new Intent(MainActivity.this, RecordActivity.class);
+                startActivity(intent);
             default:
                 break;
         }
---
+++ app/src/main/java/net/devwiki/study/RecordActivity.java
@@ -0,0 +1,99 @@
+package net.devwiki.study;
+
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import net.devwiki.study.log.DevLog;
+
+public class RecordActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button startButton;
+    private Button stopButton;
+    private AudioRecord audioRecord;
+    private boolean isRecording = false;
+    private int bufferSize;
+    private byte[] dataBuffer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+
+        startButton = (Button) findViewById(R.id.record_start);
+        stopButton = (Button) findViewById(R.id.record_stop);
+
+        initRecord();
+
+        boolean result = checkPermission("android.permission.RECORD_AUDIO");
+        DevLog.d("result:" + result);
+    }
+
+    private void initRecord(){
+        bufferSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        dataBuffer = new byte[bufferSize];
+        audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, 44100,
+                AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.record_start:
+                startRecord();
+                break;
+            case R.id.record_stop:
+                stopRecord();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void startRecord(){
+        DevLog.d("start record!!!");
+        isRecording = true;
+        audioRecord.startRecording();
+        new Thread(volumeRun).start();
+    }
+
+    private void stopRecord(){
+        DevLog.d("stop record!!!");
+        isRecording = false;
+        audioRecord.stop();
+    }
+
+    private Runnable volumeRun = new Runnable() {
+        @Override
+        public void run() {
+            while(isRecording){
+                int size = audioRecord.read(dataBuffer, 0, bufferSize);
+                DevLog.d("size:" + size);
+                DevLog.d("data:" + byte2hex(dataBuffer));
+            }
+        }
+    };
+
+    private static String byte2hex(byte [] buffer){
+        String h = "";
+
+        for(int i = 0; i < buffer.length; i++){
+            String temp = Integer.toHexString(buffer[i] & 0xFF);
+            if(temp.length() == 1){
+                temp = "0" + temp;
+            }
+            h = h + " "+ temp;
+        }
+        return h;
+    }
+
+    private boolean checkPermission(String permission) {
+        int res = checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
+    }
+}
